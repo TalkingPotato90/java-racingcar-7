33d8bc6943b4d09a3e61998ef2d853b3a1dcd5c2
TalkingPotato90 <star901210@hanmail.net>
2024-11-25 09:41:44 +0900

    feat: 시도 횟수 입력 받기

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -5,6 +5,8 @@ public class Main {
      * 임시 구현
      */
     public void startApplication(){
-
+        InputView inputView = new InputView();
+        inputView.receiveCarNames();
+        inputView.receiveTryCount();
     }
 }
