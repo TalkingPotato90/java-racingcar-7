f45c2d0bc4aefbca5a404a3a1ad0e5797abe283c
TalkingPotato90 <star901210@hanmail.net>
2024-11-25 09:18:36 +0900

    feat: 초기 설정

---
+++ src/main/java/racingcar/Main.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Main {
+    /**
+     * 임시 구현
+     */
+    public void startApplication(){
+    }
+}
---
+++ src/test/java/racingcar/MainTest.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("")
+class MainTest {
+
+}
